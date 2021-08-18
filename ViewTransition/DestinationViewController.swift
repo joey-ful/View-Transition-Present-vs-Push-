37b8809ce7661454305f8b972439b843e10a8734
joey-ful <isjoeyful@gmail.com>
2021-08-18 14:52:35 +0900

    done

--- ViewTransition/DestinationViewController.swift
+++ ViewTransition/DestinationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DestinationViewController.swift
 //  ViewTransition
 //
 //  Created by 홍정아 on 2021/08/18.
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DestinationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
-
